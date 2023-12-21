fe8183630796c5e20dab277b485469ee7b0a3796
LazY <darshitmistry2@gmail.com>
2023-12-21 11:21:05 +0530

    style(sidebar): added list of user and skeleton loaded

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import getSelf from "@/lib/auth-service";
 
 const getRecommended = async () => {
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
     const user = await db.user.findMany({
         orderBy : {
             createdAt : "desc"
