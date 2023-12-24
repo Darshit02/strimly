14e05f6f1ccfe9ee69f87b5fd995708a4c6081b6
LazY <darshitmistry2@gmail.com>
2023-12-24 10:33:01 +0530

    fix:showing self like one user

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
-import Is
 import { useEffect, useState } from "react";
 import { ToggleSkeleton } from "./Toggle";
 import { RecommendedSkeleton } from "./Recommended";
